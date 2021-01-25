9a01ac339527188653e509094ad95a635b978a0f
Amarvir <amarvirgill06@gmail.com>
2021-01-25 18:38:32 +0530

    Initial Commit

---
+++ mapPractise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapPractise
+//
+//  Created by Amarvir Mac on 25/01/21.
+//  Copyright © 2021 Amarvir Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
